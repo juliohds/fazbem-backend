24d27973346405da7f9e4ce2dab741e93b5963f8
juliohds <tecjuliohenrique@gmail.com>
2018-11-25 09:56:30 -0200

    rotas e controller

--- app/usuario.php
+++ app/usuario.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class usuario extends Model
 {
-    //
+    protected $fillable = [
+        'id', 'nome', 'email', 'senha'
+    ];
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at','deleted_at'
+    ];
 }
