3e8536466f015a00e73b16862cf8fb2183a4f458
juliohds <tecjuliohenrique@gmail.com>
2018-11-25 10:08:22 -0200

    configracoes auth api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+use App\Usuario;
 
 class UserController extends Controller
 {
@@ -13,28 +13,28 @@ class UserController extends Controller
      * @return void
      */
     public function show($id){
-        $user = User::with('productCategory')->find($id);
+        $user = Usuario::with('productCategory')->find($id);
         return response()->json($user);
     }
     public function create(Request $request){
-        $user = new ProUserduct();
+        $user = new Usuario();
         $user->fill($request->all());
         $user->save();
         return response()->json($user);
     }
     public function update(Request $request, $id){
-        $user = User::find($id);
+        $user = Usuario::find($id);
         $user->fill($request->all());
         $user->update();
         return response()->json($user);
     }
     public function destroy($id){
-        $user = User::find($id);
+        $user = Usuario::find($id);
         $user->delete();
         return response()->json($user);
     }
     public function showAll(){
-        $user = User::all();
+        $user = Usuario::all();
         return response()->json($user);
     }
 }
